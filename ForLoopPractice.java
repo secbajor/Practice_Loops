851d92efd5c337fcc8fca7a0656186cf458d1b7e
secbajor <secbajor@gmail.com>
2013-08-15 16:18:24 -0700

    added 8.15.13

---
+++ ForLoopPractice.java
@@ -0,0 +1,40 @@
+
+public class ForLoopPractice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//we can INCREMENT a variable by one using i++ or i=i+1  
+		 
+		System.out.println("This for loop prints the numbers 1 to 10:");
+		for(int i=1; i<11; i++)
+		{
+			System.out.print(i); 
+		}
+		
+				
+		System.out.println("This for loop prints the numbers 10 to 1:");
+		for(initial value; condition; step)
+		{
+			
+		}
+		
+		//write a for loop that prints the even numbers only, 1 to 10 
+		
+		//write a for loop that prints the odd numbers only, 1 to 10 
+		
+		int i = 1; 
+		while(i<10)
+		{
+			System.out.print(i); 
+			i++; 
+		}
+		
+		//write a while loop that prints the even numbers only, 1 to 10 
+		
+		
+	}//end main 
+
+}//end class
